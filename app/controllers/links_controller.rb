1e204cbf00050211edd1c406839758979c417073
YaponEran <erannodepp9.4@gmail.com>
2021-10-12 05:41:11 +0600

    user can delete links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,8 +1,11 @@
 class LinksController < ApplicationController
   before_action :authenticate_user!
   before_action :find_link, only: [:destroy]
-  
+
   def destroy
+    if current_user.author_of?(@link.linkable)
+      @link.destroy
+    end
   end
 
   private
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,14 @@
 class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
-  
+
   validates :name, :url, presence: true
+  # validates :url, url: true
+
+  def gist?
+    url.include?("gist.github.com")
+  end
+
+  def git_hash
+    url.split('/').last
+  end
 end
