b1116fb1e8f877ebc36bbafa4d2bfa0d64e78d6f
YaponEran <erannodepp9.4@gmail.com>
2021-10-12 05:53:33 +0600

    User can edit links

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,9 @@ class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
-  # validates :url, url: true
+  validates :url,
+            format: { with: URI::DEFAULT_PARSER.make_regexp },
+            if: ->(link) { link.url.present? }
 
   def gist?
     url.include?("gist.github.com")
